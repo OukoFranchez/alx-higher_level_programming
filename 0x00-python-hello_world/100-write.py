3da8f790cc825c8da369d806552373c116e1a7fc
OukoFranchez <oukofranchez@gmail.com>
2023-08-07 20:48:26 +0300

    update solution to task 11

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -3,7 +3,7 @@ import sys
 
 
 def main():
-    message = "and that piece of art is useful -Dora Korpar, 2015-10-19"
+    message = "and that piece of art is useful - Dora Korpar, 2015-10-19"
     sys.stderr.write(message + "\n")
     sys.exit(1)
 
