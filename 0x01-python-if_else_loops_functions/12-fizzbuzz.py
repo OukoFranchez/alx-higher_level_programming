e659ffa18b0a2bb9e61c9fbb0b71c5ee7af3afe1
OukoFranchez <oukofranchez@gmail.com>
2023-08-08 13:32:39 +0300

    add solution to task 12

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    fizz = "Fizz"
+    buzz = "Buzz"
+    fiz_and_buzz = "FizzBuzz"
+    for i in range(1, 101):
+        if i % 3 == 0:
+            print(f"{fizz} ", end="")
+        elif i % 5 == 0:
+            print(f"{buzz} ", end="")
+        elif (i % 3 == 0) and (i % 5 == 0):
+            print(f"{fiz_and_buzz} ", end="")
+        else:
+            print(f"{i} ", end="")
