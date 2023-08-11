7d2b0b623c10318e43d5ce7d7c0d72282fe09812
OukoFranchez <oukofranchez@gmail.com>
2023-08-11 21:05:09 +0300

    update-line7 solution to task 6

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,7 +4,8 @@ from sys import argv
 
 if __name__ == "__main__":
     if len(argv) < 3:
-        print("Usage:", argv[0], "<a> <operator> <b>")
+        # print("Usage:", argv[0], "<a> <operator> <b>")
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         quit(1)
 
     operators = ['+', '-', '*', '/']
