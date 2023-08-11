93ad27e31dd30c4593b68f075b084e042b631d16
OukoFranchez <oukofranchez@gmail.com>
2023-08-11 21:00:05 +0300

    add solution to task 6

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+from sys import argv
+
+if len(argv) < 3:
+    print("Usage:", argv[0], "<a> <operator> <b>")
+    quit(1)
+
+operators = ['+', '-', '*', '/']
+
+if argv[2] not in operators:
+    print("Unknown operator. Available operators: +, -, * and /")
+    quit(1)
+
+a = int(argv[1])
+b = int(argv[3])
+
+if argv[2] == "+":
+    print(a, argv[2], b, "=", add(a, b))
+elif argv[2] == "-":
+    print(a, argv[2], b, "=", sub(a, b))
+elif argv[2] == "*":
+    print(a, argv[2], b, "=", mul(a, b))
+else:
+    print(a, argv[2], b, "=", div(a, b))
