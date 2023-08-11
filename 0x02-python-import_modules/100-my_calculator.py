02334dee541c044ddc61e54a3de77547cc516b49
OukoFranchez <oukofranchez@gmail.com>
2023-08-11 21:02:10 +0300

    update solution to task 6

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,24 +2,25 @@
 from calculator_1 import add, sub, mul, div
 from sys import argv
 
-if len(argv) < 3:
-    print("Usage:", argv[0], "<a> <operator> <b>")
-    quit(1)
+if __name__ == "__main__":
+    if len(argv) < 3:
+        print("Usage:", argv[0], "<a> <operator> <b>")
+        quit(1)
 
-operators = ['+', '-', '*', '/']
+    operators = ['+', '-', '*', '/']
 
-if argv[2] not in operators:
-    print("Unknown operator. Available operators: +, -, * and /")
-    quit(1)
+    if argv[2] not in operators:
+        print("Unknown operator. Available operators: +, -, * and /")
+        quit(1)
 
-a = int(argv[1])
-b = int(argv[3])
+    a = int(argv[1])
+    b = int(argv[3])
 
-if argv[2] == "+":
-    print(a, argv[2], b, "=", add(a, b))
-elif argv[2] == "-":
-    print(a, argv[2], b, "=", sub(a, b))
-elif argv[2] == "*":
-    print(a, argv[2], b, "=", mul(a, b))
-else:
-    print(a, argv[2], b, "=", div(a, b))
+    if argv[2] == "+":
+        print(a, argv[2], b, "=", add(a, b))
+    elif argv[2] == "-":
+        print(a, argv[2], b, "=", sub(a, b))
+    elif argv[2] == "*":
+        print(a, argv[2], b, "=", mul(a, b))
+    else:
+        print(a, argv[2], b, "=", div(a, b))
