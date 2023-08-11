0f7a6c3f06c2fa42e7f4d81052c1928392f56724
OukoFranchez <oukofranchez@gmail.com>
2023-08-11 19:46:56 +0300

    update solution to task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
+import hidden_4
+
 if __name__ == "__main__":
-    from hidden_4 import *
-    arr = dir()
-    for i in range(0, len(arr)):
-        if arri[i][0:2] != "__":
-            print("{}.format(arr[i])")
+    for name in dir(hidden_4):
+        if name[0] != '_':
+            print("{}".format(name))
