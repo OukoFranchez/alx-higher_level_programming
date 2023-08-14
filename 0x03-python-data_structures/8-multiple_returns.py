8af43115632a06736d73b6572aac6999499542c3
OukoFranchez <oukofranchez@gmail.com>
2023-08-14 18:38:07 +0300

    add solution to task 8

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    sentence_tuple = tuple(sentence)
+    if sentence_tuple == ():
+        return (None)
+    else:
+        length = len(sentence_tuple)
+        first_char = sentence_tuple[0]
+        return (length, first_char)
