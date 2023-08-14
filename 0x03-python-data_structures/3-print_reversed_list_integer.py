43ff292d8bb694328fbf06610b923d6438fbe8a2
OukoFranchez <oukofranchez@gmail.com>
2023-08-14 12:06:00 +0300

    update-for instead of while  solution to task 3

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     my_list.reverse()
-    i = 0
-    while i < len(my_list):
-        print("{:d}".format(my_list[i]))
-        i += 1
+    # i = 0
+    # while i < len(my_list):
+    #     print("{:d}".format(my_list[i]))
+    #     i += 1
+    for i in my_list:
+        print("{:d}".format(i))
