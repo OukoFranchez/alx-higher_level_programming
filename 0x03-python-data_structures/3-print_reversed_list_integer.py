fc73cae4a4de426231d3f55684bca98275ce59d8
OukoFranchez <oukofranchez@gmail.com>
2023-08-14 11:51:36 +0300

    add solution to task 3

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    my_list.reverse()
+    i = 0
+    while i < len(my_list):
+        print("{:d}".format(my_list[i]))
+        i += 1
