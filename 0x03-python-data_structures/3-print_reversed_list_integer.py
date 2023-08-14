a413970d6f032e8323c103cda5efadb74a0b4c52
OukoFranchez <oukofranchez@gmail.com>
2023-08-15 02:41:55 +0300

    update-create custom reverse  solution to task 3

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    my_list.reverse()
+    # my_list.reverse()
     # i = 0
     # while i < len(my_list):
     #     print("{:d}".format(my_list[i]))
     #     i += 1
-    for i in my_list:
-        print("{:d}".format(i))
+    # for i in my_list:
+    i = len(my_list) - 1
+    while i >= 0:
+        print("{:d}".format(my_list[i]))
+        i -= 1
