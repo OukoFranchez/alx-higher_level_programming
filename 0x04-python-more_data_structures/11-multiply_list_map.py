7386c7f25d6437ef154111a2a95742651c95f9e9
OukoFranchez <oukofranchez@gmail.com>
2023-08-17 02:58:26 +0300

    add solution to task 11

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    multiplied = map(lambda i: i * number, my_list)
-    # converting  the map object to a list
-    new_list = list(multiplied)
-    return new_list
+    return list(map(lambda i: i * number, my_list))
