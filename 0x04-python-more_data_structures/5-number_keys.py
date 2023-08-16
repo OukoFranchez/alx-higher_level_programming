86672e14af79f689d7545398754dd789efad9667
OukoFranchez <oukofranchez@gmail.com>
2023-08-17 01:30:29 +0300

    add-update approach-2 solution to task 5

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
-    sum = 0
-    i = 0
-    for key in a_dictionary:
-        sum += i
-        i += 1
-    return sum
+    return len(a_dictionary.keys())
