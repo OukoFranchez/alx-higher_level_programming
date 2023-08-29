5d0b1ccf4fb8b56f5ce7ed32432966f64db43820
OukoFranchez <oukofranchez@gmail.com>
2023-08-29 03:42:52 +0300

    add solution to task 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list(my_list = [], x = 0):
+    printed = 0
+    try:
+        for i in range(x):
+            print(my_list[i], end="")
+            printed += 1
+    # handling trying to access an index beyond the list len
+    except IndexError:
+        pass
+    finally:
+        print()
+    return (printed)
