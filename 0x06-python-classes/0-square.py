782aba4bfa4c0ae0aef63d055517f0e9638b20ac
OukoFranchez <oukofranchez@gmail.com>
2023-08-30 01:07:18 +0300

    add more docstring

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+
+"""This module provides classes for handling certain task 0.
+
+Classes:
+    Square: Defines a square.
+"""
+
+
 class Square:
     """
     A class representing a square.
