2b778af83b391a28b9478ac872f829b33276f35c
OukoFranchez <oukofranchez@gmail.com>
2023-09-04 17:36:31 +0300

    add solution to task 0

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module provides class for task 0
+classes:
+    Rectangle - defines a Rectangle
+    """
+
+
+class Rectangle:
+    """Defines a rectangular shape"""
+    pass
