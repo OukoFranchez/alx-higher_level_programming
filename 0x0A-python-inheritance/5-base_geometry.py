8effb91151aa5c4ad8545aba3bcd03b7b2db0555
OukoFranchez <oukofranchez@gmail.com>
2023-09-12 02:18:36 +0300

    add solution to task 5

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Module for task 5"""
+
+
+class BaseGeometry:
+    """
+    This is the BaseGeometry class, which serves as a template for creating other geometry classes.
+
+    Example Usage:
+    ```
+    # Creating an instance of the BaseGeometry class
+    geometry = BaseGeometry()
+    ```
+
+    Methods:
+    - No specific methods implemented.
+
+    Fields:
+    - No specific fields defined.
+    """
+
+    pass
