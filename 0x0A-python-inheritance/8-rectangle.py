a610a10246964697813a7ca6c15ee0b248559957
OukoFranchez <oukofranchez@gmail.com>
2023-09-12 05:09:11 +0300

    import parent class

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -2,42 +2,7 @@
 """Module for task 8"""
 
 
-class BaseGeometry:
-    """
-    A base class that provides a blueprint for other geometry classes.
-
-    Methods:
-    - area(): Raises an exception since it
-    is not implemented in the base class.
-    - integer_validator(name, value): Validates if the value is
-    an integer and greater than 0.
-    Raises a TypeError or ValueError if the validation fails.
-    """
-
-    def area(self):
-        """
-        Raises:
-            Exception: This method is not implemented in the base class.
-        """
-        raise Exception("area() is not implemented")
-
-    @staticmethod
-    def integer_validator(name, value):
-        """
-        Validates if the value is an integer and greater than 0.
-
-        Args:
-            name (str): The name of the value being validated.
-            value (int): The value to be validated.
-
-        Raises:
-            TypeError: If the value is not an integer.
-            ValueError: If the value is less than or equal to 0.
-        """
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-        elif value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
