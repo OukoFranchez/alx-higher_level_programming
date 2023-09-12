53f351f9b4fd6f59b3413a4fb6177c2ec350f8bf
OukoFranchez <oukofranchez@gmail.com>
2023-09-12 04:58:02 +0300

    add solution to task 12

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""
+    0x0A. Python - Inheritance, Task 12
+"""
+
+
+class MyInt(int):
+    """ class that inherits from int and inverts
+        the == and != operators
+    """
+    def __eq__(self, other):
+        """
+            == operator, return !=
+        """
+        return super().__ne__(other)
+
+    def __ne__(self, other):
+        """
+            != operator, return ==
+        """
+        return super().__eq__(other)
