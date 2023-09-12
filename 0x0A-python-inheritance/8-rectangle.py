56685394675c8f06e950aa56f00be4a204495360
OukoFranchez <oukofranchez@gmail.com>
2023-09-12 05:02:30 +0300

    refactor task 8

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -50,7 +50,7 @@ class Rectangle(BaseGeometry):
     """
 
     def __init__(self, width, height):
-        self.__width = width
-        self.__height = height
         super().integer_validator("width", width)
         super().integer_validator("height", height)
+        self.__width = width
+        self.__height = height
