85ecf8938f16b3786d31f0c19f79dafd0be29138
OukoFranchez <oukofranchez@gmail.com>
2023-09-12 04:29:32 +0300

    print using .format

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -34,6 +34,6 @@ class BaseGeometry:
             ValueError: If the value is less than or equal to zero.
         """
         if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+            raise ValueError("{} must be greater than 0".format(name))
