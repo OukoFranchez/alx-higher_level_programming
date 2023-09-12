babc3258ee3fbceebba8ce19d3db1f161e2b0822
OukoFranchez <oukofranchez@gmail.com>
2023-09-12 03:48:30 +0300

    add solution to task 7

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Module for task 7"""
+
+
+class BaseGeometry:
+    """
+    A base class that provides a blueprint for other geometry classes.
+
+    Methods:
+    - area(): Placeholder method to be overridden by subclasses
+                for calculating the area of a specific geometry.
+    - integer_validator(name, value): Validates that a given value is
+                an integer and greater than zero.
+    """
+
+    def area(self):
+        """
+        Placeholder method to be overridden by subclasses
+        for calculating the area of a specific geometry.
+        Raises:
+            Exception: This method should be overridden by subclasses.
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Validates that a given value is an integer and greater than zero.
+        Args:
+            name (str): The name of the value being validated.
+            value (int): The value to be validated.
+        Raises:
+            TypeError: If the value is not an integer.
+            ValueError: If the value is less than or equal to zero.
+        """
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+        elif value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
