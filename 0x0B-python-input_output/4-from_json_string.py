7fbaeb13357b6dd4a3cc0815b0941ff02320aee2
OukoFranchez <oukofranchez@gmail.com>
2023-09-13 00:13:40 +0300

    add solution to task 4

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Module for task 4"""
+
+from json import loads
+
+
+def to_json_string(my_str):
+    """
+    Convert a string into a JSON object.
+
+    Args:
+        my_str (str): A string representing a JSON object.
+
+    Returns:
+        str: A string representing the JSON object.
+
+    Example:
+        >>> my_str = '{"name": "John", "age": 30}'
+        >>> json_string = to_json_string(my_str)
+        >>> print(json_string)
+        {"name": "John", "age": 30}
+    """
+
+    return loads(my_str)
