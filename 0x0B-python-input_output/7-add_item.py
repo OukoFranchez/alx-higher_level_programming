a3fcce6a21fa0a147495ca9d41a29e9fa6ee529e
OukoFranchez <oukofranchez@gmail.com>
2023-09-13 05:53:43 +0300

    add solution to task 7 - pycodestyle

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,28 +3,30 @@
 Contains the "save_to_json_file" function
 """
 
-from json import dump
+import json
 
 
 def save_to_json_file(my_obj, filename):
     """
-    Write an object to a text file in JSON format.
+    Save an object as a JSON file.
 
     Args:
-        my_obj: The object to be written to the file.
-        filename: The name of the file to write the object to.
+        my_obj (object): The object to be saved as a JSON file.
+        filename (str): The name of the file to save the JSON data to.
 
     Returns:
-        None. The object is written to the file specified by `filename`.
+        None
 
-    Example:
-        save_to_json_file(my_obj, "output.json")
+    Example Usage:
+        my_obj = {"name": "John", "age": 30}
+        filename = "data.json"
+        save_to_json_file(my_obj, filename)
 
-    This function opens the file specified by `filename` in
-    write mode with UTF-8 encoding.
-    It uses the `dump` function from the `json` module
-    to write the JSON representation of `my_obj` to the file.
+    Code Analysis:
+        This function opens the file specified by `filename`
+        in write mode with UTF-8 encoding.
+        It then uses the `json.dump` function to
+        write the JSON representation of `my_obj` to the file.
     """
-    # Open the file in write mode with UTF-8 encoding
     with open(filename, 'w', encoding='utf-8') as f:
-        dump(my_obj, f)
+        json.dump(my_obj, f)
