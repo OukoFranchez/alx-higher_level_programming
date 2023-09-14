b1d041b78f8d02ee9bccf5ebba00cc34d9581e6c
OukoFranchez <oukofranchez@gmail.com>
2023-09-14 03:06:14 +0300

    refactor task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,26 +7,6 @@ import json
 
 
 def save_to_json_file(my_obj, filename):
-    """
-    Save an object as a JSON file.
-
-    Args:
-        my_obj (object): The object to be saved as a JSON file.
-        filename (str): The name of the file to save the JSON data to.
-
-    Returns:
-        None
-
-    Example Usage:
-        my_obj = {"name": "John", "age": 30}
-        filename = "data.json"
-        save_to_json_file(my_obj, filename)
-
-    Code Analysis:
-        This function opens the file specified by `filename`
-        in write mode with UTF-8 encoding.
-        It then uses the `json.dump` function to
-        write the JSON representation of `my_obj` to the file.
-    """
+    """writes an Object to a text file, using a JSON representation"""
     with open(filename, 'w', encoding='utf-8') as f:
         json.dump(my_obj, f)
