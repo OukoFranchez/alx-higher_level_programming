07bd40a8a1bd69802b83a415aea3b82e7a2f02cc
OukoFranchez <oukofranchez@gmail.com>
2023-09-19 02:45:10 +0300

    fix: correct cases for output error

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -63,7 +63,7 @@ class Rectangle(Base):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value <= 0:
-            raise ValueError("idth must be > 0")
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
