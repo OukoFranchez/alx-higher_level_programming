6d36471ae6a84340f8305ac0da0ed67b2bfa5195
OukoFranchez <oukofranchez@gmail.com>
2023-09-19 02:36:41 +0300

    feat: update rectangle class for task 3

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -62,8 +62,8 @@ class Rectangle(Base):
         """
         if not isinstance(value, int):
             raise TypeError("Width must be an integer")
-        if value < 0:
-            raise ValueError("Width must be greater than 0")
+        if value <= 0:
+            raise ValueError("Width must be > 0")
         self.__width = value
 
     @property
@@ -91,8 +91,8 @@ class Rectangle(Base):
         """
         if not isinstance(value, int):
             raise TypeError("Height must be an integer")
-        if value < 0:
-            raise ValueError("Height must be greater than 0")
+        if value <= 0:
+            raise ValueError("Height must be > 0")
         self.__height = value
 
     @property
@@ -121,7 +121,7 @@ class Rectangle(Base):
         if not isinstance(value, int):
             raise TypeError("x must be an integer")
         if value < 0:
-            raise ValueError("x must be greater than 0")
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
@@ -150,5 +150,5 @@ class Rectangle(Base):
         if not isinstance(value, int):
             raise TypeError("y must be an integer")
         if value < 0:
-            raise ValueError("y must be greater than 0")
+            raise ValueError("y must be >= 0")
         self.__y = value
