b50063eb42f75a43835a37e9b7799a5f42de7efc
OukoFranchez <oukofranchez@gmail.com>
2023-11-13 21:20:13 +0300

    refactor task 3 with an alternative approach

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,20 +1,7 @@
 #!/usr/bin/node
 
-let argCount = 0;
-
-// Loop until process.argv[argCount] is undefined
-while (process.argv[argCount] !== undefined) {
-  argCount++;
-}
-
-// console.log('Number of arguments:', argCount);
-
-if (argCount <= 2) {
+if (process.argv[2] === undefined) {
   console.log('No argument');
 } else {
-  // Print each argument
-  // for (let index = 2; index < argCount; index++) {
-  //   console.log(`${process.argv[index]}`);
-  // }
-  console.log('', process.argv[2]);
+  console.log(process.argv[2]);
 }
