11f48cbcc31174d5265e7a77794469b8fd0cb54f
OukoFranchez <oukofranchez@gmail.com>
2023-11-14 01:06:13 +0300

    feat: add solution to task 6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+const multiLanguagesArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let i = 0; i < multiLanguagesArray.length; i++) {
+  console.log(multiLanguagesArray[i]);
+}
