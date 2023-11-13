648e21f7df16f6973cea9ec5caf3b9762957b924
OukoFranchez <oukofranchez@gmail.com>
2023-11-13 23:47:20 +0300

    fix: use parseInt to truncate decimal to whole number

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,8 +2,8 @@
 
 if (process.argv.length < 3) {
   console.log('Not a number');
-} else if (!isNaN(+process.argv[2])) {
-  console.log(`My Number: ${+process.argv[2]}`);
+} else if (!isNaN(parseInt(process.argv[2], 10))) {
+  console.log(`My number: ${parseInt(process.argv[2], 10)}`);
 } else {
   console.log('Not a number');
 }
