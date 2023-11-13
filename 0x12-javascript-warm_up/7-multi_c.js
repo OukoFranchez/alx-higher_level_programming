2f13256a2e30847de2cdbc2b91277ca0c41b425c
OukoFranchez <oukofranchez@gmail.com>
2023-11-14 01:41:40 +0300

    feat: follow semistandard guidelines

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
-let string = "C is fun";
-let convertedNumber = parseInt(process.argv[2], 10);
+const string = 'C is fun';
+const convertedNumber = parseInt(process.argv[2], 10);
 
 if (process.argv.length < 3 || isNaN(convertedNumber)) {
   console.log('Missing number of occurrences');
-}else {
+} else {
   for (let i = 0; i < convertedNumber; i++) {
     console.log(string);
   }
