b3b8f7c4313ed14d34ec3e6ca415cef5a3ed5235
OukoFranchez <oukofranchez@gmail.com>
2023-11-16 03:51:27 +0300

    feat: add solution to task 11

---
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const list = require('./100-data.js').list;
+
+const mappedList = list.map(function (value, index) {
+  return value * index;
+});
+
+console.log(list);
+console.log(mappedList);
