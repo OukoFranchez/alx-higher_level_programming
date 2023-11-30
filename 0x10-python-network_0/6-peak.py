2abd1baf160d540a3ca7cd81d5bd4ef143d99bc2
OukoFranchez <96543749+OukoFranchez@users.noreply.github.com>
2023-12-01 01:55:23 +0300

    refactor to improve efficiency

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,35 +3,34 @@
 Module for task 6
 """
 
-
-def find_peak(lst):
+def find_peak(list_int):
     """
     Find the peak in a list of integers.
 
     Args:
-        lst (list): List of integers.
+        list_int (list): List of integers.
 
     Returns:
         int or None: Peak element if found, None otherwise.
     """
     # Check for an empty list
-    if not lst:
+    if list_int == []:
         return None
     # Check for a single-element list
-    elif len(lst) == 1:
-        return lst[0]
+    elif len(list_int) == 1:
+        return list_int[0]
     # Check for a two-element list
-    elif len(lst) == 2:
-        return max(lst[0], lst[1])
+    elif len(list_int) == 2:
+        return list_int[0] if list_int[0] > list_int[1] else list_int[1]
     else:
         # Iterate through the list
-        for i in range(1, (len(lst) + 1) // 2):
-            back = len(lst) - i - 1
-            # Check if current element is a peak
-            if lst[i] >= lst[i - 1] and lst[i] >= lst[i + 1]:
-                return lst[i]
-            # Check if corresponding element from the end is a peak
-            elif lst[back] >= lst[back - 1] and lst[back] >= lst[back + 1]:
-                return lst[back]
+        for i in range(1, (len(list_int) + 1) // 2):
+            back = len(list_int) - i - 1
+            # Check if the current element is a peak
+            if list_int[i] >= list_int[i - 1] and list_int[i] >= list_int[i + 1]:
+                return list_int[i]
+            # Check if the corresponding element from the end is a peak
+            elif list_int[back] >= list_int[back - 1] and list_int[back] >= list_int[back + 1]:
+                return list_int[back]
     # No peak found
     return None
