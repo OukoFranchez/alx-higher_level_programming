f96c90873a20e896ce0331a1551e625a1eb0c903
OukoFranchez <96543749+OukoFranchez@users.noreply.github.com>
2023-12-04 20:44:43 +0300

    refactor task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,11 +6,11 @@ A python script that fetches https://intranet.hbtn.io/status
 
 import urllib.request
 
-
 if __name__ == "__main__":
-    with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
-        output = response.read()
-        print('Body response:')
-        print('\t- type: {}'.format(type(output)))
-        print('\t- content: {}'.format(output))
-        print('\t- utf8 content: {}'.format(output.decode('utf-8')))
+    request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
