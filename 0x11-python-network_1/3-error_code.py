571dc8ae97bc050d332b76994177e27e57fecd36
OukoFranchez <96543749+OukoFranchez@users.noreply.github.com>
2023-12-04 20:20:56 +0300

    style: correct indentation errors in pycodestyle

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
 """
-Python script that takes in a URL, sends a request to the URL and displays the body of the response (decoded in utf-8).
+Python script that takes in a URL,
+sends a request to the URL and displays
+the body of the response (decoded in utf-8).
 """
 
 
@@ -11,7 +13,7 @@ from sys import argv
 
 if __name__ == "__main__":
     try:
-         with urllib.request.urlopen(argv[1]) as response:
+        with urllib.request.urlopen(argv[1]) as response:
             print(response.read().decode('utf-8'))
     except urllib.error.HTTPError as e:
         print('Error code: {}'.format(e.code))
